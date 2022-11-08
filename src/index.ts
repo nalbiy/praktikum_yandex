23424dfdb6d345b3c4581a26fe0480370d24bc10
KozinNY <n888n@inbox.ru>
2022-11-08 20:04:35 +0300

    try 1

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,43 @@
-import NotFoundPage from './pages/err/404';
-import '../public/cssout/mainin.css';
-import '../public/cssout/chat.css';
-import '../public/cssout/btn.css';
-import '../public/cssout/form.css';
+console.log('yeeeeeeeeeeeeeeee');
 
-import Chat from './pages/chat/ChatPage';
-import ContactPage from './pages/contacts/ContactPage';
-import RenderDom from './modules/RenderDom';
-import formPage from './pages/forms/Forms';
-import NavbarItems from './component/Navbar/Navbar';
+let main = document.getElementById('main');
+main.innerHTML = 'yeeeeeeeeeeeeeeeeeee';
 
-const path = window.location.pathname;
-
-RenderDom('#navbar', NavbarItems());
-
-switch (path) {
-  case '/contacts':
-    RenderDom('#main', ContactPage());
-    break;
-  case '/chat':
-    RenderDom('#main', Chat());
-    break;
-  case '/login':
-    RenderDom('#main', formPage(path));
-    break;
-  case '/reg':
-    RenderDom('#main', formPage(path));
-    break;
-  case '/settings':
-    RenderDom('#main', formPage(path));
-    break;
-  default:
-    NotFoundPage();
-    break;
-}
+//
+//
+// import NotFoundPage from './pages/err/404';
+// import '../public/cssout/mainin.css';
+// import '../public/cssout/chat.css';
+// import '../public/cssout/btn.css';
+// import '../public/cssout/form.css';
+//
+// import Chat from './pages/chat/ChatPage';
+// import ContactPage from './pages/contacts/ContactPage';
+// import RenderDom from './modules/RenderDom';
+// import formPage from './pages/forms/Forms';
+// import NavbarItems from './component/Navbar/Navbar';
+//
+// const path = window.location.pathname;
+//
+// RenderDom('#navbar', NavbarItems());
+//
+// switch (path) {
+//   case '/contacts':
+//     RenderDom('#main', ContactPage());
+//     break;
+//   case '/chat':
+//     RenderDom('#main', Chat());
+//     break;
+//   case '/login':
+//     RenderDom('#main', formPage(path));
+//     break;
+//   case '/reg':
+//     RenderDom('#main', formPage(path));
+//     break;
+//   case '/settings':
+//     RenderDom('#main', formPage(path));
+//     break;
+//   default:
+//     NotFoundPage();
+//     break;
+// }
